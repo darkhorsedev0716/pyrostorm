73d2d5f74f5f5929638243090eb3ea33d2fb0878
darkhorse0716 <goldtrump1015@gmail.com>
2013-05-22 00:04:46 -0400

    Added domain variable + few new functions

--- example.js
+++ example.js
@@ -13,3 +13,11 @@ digitalocean.GetDroplet({'droplet': '196645'}, function(data) {
 digitalocean.NewDroplet({'name': 'test', 'size': '66', 'image': '2676', 'region': '1', 'virtio': '1'}, function(data) {
   process.stdout.write(data);
 });
+
+digitalocean.RebootDroplet({'droplet': '196645'}, function(data) {
+  process.stdout.write(data);
+});
+
+digitalocean.PowerCycleDroplet({'droplet': '196645'}, function(data) {
+  process.stdout.write(data);
+});
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var https = require('https')
   , clientKey
-  , apiKey;
+  , apiKey
+  , domain = 'https://api.digitalocean.com/';
 
 exports.SetKeys = function(clientKey, apiKey) {
   this.clientKey = clientKey;
@@ -8,7 +9,7 @@ exports.SetKeys = function(clientKey, apiKey) {
 };
 
 exports.GetDroplets = function(callback) {
-  https.get('https://api.digitalocean.com/droplets/?client_id=' + this.clientKey + '&api_key=' + this.apiKey, function(res) {
+  https.get(domain + '/droplets/?client_id=' + this.clientKey + '&api_key=' + this.apiKey, function(res) {
     res.on('data', function(data) {
       callback(data);
     });
@@ -16,7 +17,7 @@ exports.GetDroplets = function(callback) {
 };
 
 exports.GetDroplet = function(info, callback) {
-  https.get('https://api.digitalocean.com/droplets/' + info.droplet + '?client_id=' + this.clientKey + '&api_key=' + this.apiKey, function(res) {
+  https.get(domain + 'droplets/' + info.droplet + '?client_id=' + this.clientKey + '&api_key=' + this.apiKey, function(res) {
     res.on('data', function(data) {
       callback(data);
     });
@@ -24,9 +25,25 @@ exports.GetDroplet = function(info, callback) {
 };
 
 exports.NewDroplet = function(info, callback) {
-  https.get('https://api.digitalocean.com/droplets/new??client_id=' + this.clientKey + '&api_key=' + this.apiKey + '&name=' + info.name 
+  https.get(domain + 'droplets/new??client_id=' + this.clientKey + '&api_key=' + this.apiKey + '&name=' + info.name 
   + '&size_id=' + info.size + '&image_id=' + info.image + '&region_id=' + info.region + '&client_id=' + this.clientKey 
   + '&virtio=' + info.virtio + '&ssh_key_ids=' + info.sshKey, function(res) {
+    res.on('data', function(data) {
+      callback(data);
+    });
+  });
+};
+
+exports.RebootDroplet = function(info, callback) {
+  https.get(domain + 'droplets/' + info.droplet + '/reboot/?client_id=' + this.clientKey + '&api_key=' + this.apiKey, function(res) {
+    res.on('data', function(data) {
+      callback(data);
+    });
+  });
+};
+
+exports.PowerCycleDroplet = function(info, callback) {
+  https.get(domain + 'droplets/' + info.droplet + '/power_cycle/?client_id=' + this.clientKey + '&api_key=' + this.apiKey, function(res) {
     res.on('data', function(data) {
       callback(data);
     });
