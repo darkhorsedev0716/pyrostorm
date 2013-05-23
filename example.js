54153c1941f61fc5e545723eead25df18041a11e
darkhorse0716 <goldtrump1015@gmail.com>
2013-05-23 03:13:33 -0400

    Added few missing functions, and almost all examples

--- example.js
+++ example.js
@@ -2,6 +2,7 @@ var digitalocean = require('./lib/index');
   
 digitalocean.SetKeys('insertClientKey', 'insertAPIKey');
 
+/*
 digitalocean.GetDroplets(function(data) {
   process.stdout.write(data);
 });
@@ -21,3 +22,72 @@ digitalocean.RebootDroplet({'droplet': '196645'}, function(data) {
 digitalocean.PowerCycleDroplet({'droplet': '196645'}, function(data) {
   process.stdout.write(data);
 });
+
+digitalocean.ShutDownDroplet({'droplet': '196645'}, function(data) {
+  process.stdout.write(data);
+});
+
+digitalocean.PowerOffDroplet({'droplet': '196645'}, function(data) {
+  process.stdout.write(data);
+});
+
+digitalocean.PowerOnDroplet({'droplet': '196645'}, function(data) {
+  process.stdout.write(data);
+});
+
+digitalocean.ResetRootPassword({'droplet': '196645'}, function(data) {
+  process.stdout.write(data);
+});
+
+digitalocean.ResizeDroplet({'droplet': '196645', 'size': '66'}, function(data) {
+  process.stdout.write(data);
+});
+
+digitalocean.TakeSnapshot({'droplet': '196645', 'name': 'test'}, function(data) {
+  process.stdout.write(data);
+});
+
+digitalocean.RestoreDroplet({'droplet': '196645', 'image': 'test'}, function(data) {
+  process.stdout.write(data);
+});
+
+digitalocean.RebuildDroplet({'droplet': '196645', 'image': 'test'}, function(data) {
+  process.stdout.write(data);
+});
+
+digitalocean.EnableDroplet({'droplet': '196645'}, function(data) {
+  process.stdout.write(data);
+});
+
+digitalocean.DisableDroplet({'droplet': '196645'}, function(data) {
+  process.stdout.write(data);
+});
+
+digitalocean.RenameDroplet({'droplet': '196645', 'name': 'test'}, function(data) {
+  process.stdout.write(data);
+});
+
+digitalocean.DestroyDroplet({'droplet': '196645'}, function(data) {
+  process.stdout.write(data);
+});
+
+digitalocean.GetRegions(function(data) {
+  process.stdout.write(data);
+});
+
+digitalocean.GetImages(function(data) {
+  process.stdout.write(data);
+});
+
+digitalocean.ShowImage({'image': '2'}, function(data) {
+  process.stdout.write(data);
+});
+
+digitalocean.DestroyImage({'image': '2'}, function(data) {
+  process.stdout.write(data);
+});
+
+digitalocean.TransferImage({'image': '2', 'region': '1'}, function(data) {
+  process.stdout.write(data);
+});
+*/
